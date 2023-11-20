b61ff16918dd97d3e6aa1733548183b7f75eac2f
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2023-11-20 19:25:24 +0000

    desafio concluído

--- src/BinaryCounter.java
+++ src/BinaryCounter.java
@@ -19,7 +19,7 @@ public class BinaryCounter {
            
         }
     
-
+        scan.close();
     
     }
 
--- src/filas/FilaBanco.java
+++ src/filas/FilaBanco.java
@@ -1,25 +1,21 @@
 package filas;
 import cliente.Cliente;
 import linked.ListaLigada;
-import list.Fila;
 
-public class FilaBanco implements Fila{
+public class FilaBanco{
     
    ListaLigada lista = new ListaLigada();
 
-   @Override
    public void enqueue(Cliente item) {
         lista.insereFim(item);
    }
 
-   @Override
    public Cliente dequeue() {
         Cliente valor = lista.buscaIndice(0);
         lista.removeInicio();
         return valor;
    }
 
-   @Override
    public boolean isEmpty() {
          if (lista.buscaIndice(0) == null){
              return true;
@@ -27,12 +23,10 @@ public class FilaBanco implements Fila{
             return false;
    }
 
-   @Override
    public int size() {
         return -1;
    }
 
-   @Override
    public String toString() {
         StringBuilder result = new StringBuilder("[");
         for (int i = 0; i < lista.size(); i++) {
